e273625ad5e0da5b0f6ca561641b5bb94402c64a
Joan Amaya <j.amayab@uniandes.edu.co>
2024-11-23 05:59:02 -0500

    Changes in histogram

--- PruebasG2Experimental/histogramGenerate.py
+++ PruebasG2Experimental/histogramGenerate.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class PulsesHistogram():
-    def __init__(self,stopNumber,binWidth, upperLimit, pathFile, saveimage):
+    def __init__(self,stopNumber,binWidth, upperLimit, pathFile, saveimage, isg2, title):
         self.stopNumber = stopNumber
         self.binWidth = binWidth
         self.upperLimit = upperLimit
         self.pathFile=pathFile
         self.saveimage=saveimage
+        self.isg2= isg2
+        self.title = title 
         
     
 
@@ -56,23 +58,36 @@ class PulsesHistogram():
         plt.figure(figsize=(12, 10))
         plt.plot(bin_edges[:-1], hist, color='blue', linestyle='-', linewidth=2)
         valueX=bin_edges[:-1]
-        # Add vertical line for mean
-        plt.axvline(avgHist, color='red', linestyle='--', linewidth=1.5, label=f'Mean ({avgHist:.2f})')
-        
-        # Add horizontal bar for standard deviation at the mean
-        plt.hlines(y=hist.max() * 0.9, xmin=avgHist - desvEst, xmax=avgHist + desvEst, color='green', linewidth=1.5, label=f'Std Dev ({desvEst:.2f})')
-        
+        if not self.isg2:
+                
+            # Add vertical line for mean
+            plt.axvline(avgHist, color='red', linestyle='--', linewidth=1.5, label=f'Mean ({avgHist:.2f})')
+            
+            # Add horizontal bar for standard deviation at the mean
+            plt.hlines(y=hist.max() * 0.9, xmin=avgHist - desvEst, xmax=avgHist + desvEst, color='green', linewidth=1.5, label=f'Std Dev ({desvEst:.2f})')
+        else:
+            avgFrequency=np.mean(hist)
+            desvestFrequency=np.std(hist)
+            plt.axhline(avgFrequency, color='red', linestyle='--', linewidth=1.5, label=f'Mean Freq: ({avgHist:.2f})')
+            plt.vlines(x=np.mean(histogramArray), ymin=avgFrequency - desvestFrequency, ymax=avgFrequency + desvestFrequency, color='green', linewidth=4, label=f'Std Dev ({desvestFrequency:.2f})')
+            avgHist=avgFrequency
+            desvEst=desvestFrequency
+
         # Mark min and max values with points on the histogram
         min_index = np.argmin(hist)
         max_index = np.argmax(hist)
-        plt.scatter( [valueX[min_index], valueX[max_index]], [min(hist), max(hist)],color='purple', marker='o', label=f'Min ({min_val}) / Max ({max_val})')
+        plt.scatter( [valueX[min_index], valueX[max_index]], [min(hist), max(hist)],color='purple', marker='o', label=f'Min ({valueX[min_index]}) / Max ({valueX[max_index]})')
         
         # Add labels and title with rounded values
         plt.xlabel('Time ('+unidades+')')
         plt.ylabel('Frequency')
+        plt.title(self.title)
         valueA= str(avgHist)+" "+unidades
         valueB= str(desvEst)+" "+unidades
-        plt.title(f'Data Difference Histogram | Mean: {avgHist:.2f} {unidades}, Std Dev: {desvEst:.2f} {unidades}, Min: {min_val}, Max: {max_val}')
+        if not self.isg2:
+            plt.title(f'Graphic:{self.title} | Mean: {avgHist:.2f} {unidades}, Std Dev: {desvEst:.2f} {unidades}, Min: {valueX[min_index]}, Max: {valueX[max_index]}')
+        else:
+            plt.title(f'Graphic:{self.title} | Mean: {avgHist:.2f}, Std Dev: {desvEst:.2f}, Min: {valueX[min_index]}, Max: {valueX[max_index]}')
         plt.ylim(bottom=0)
         
         # Add legend
@@ -93,79 +108,79 @@ class PulsesHistogram():
         else:
             return ("ps", 1)
 
-objetoGraficar= PulsesHistogram(5,500000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/DataAllStops.png")
+objetoGraficar= PulsesHistogram(5,2000000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/DataAllStops.png", False, "All Stops 20000 pulses 24Mhz")
 dataOneStop = objetoGraficar.readDataOneStop()
 dataAllStops = objetoGraficar.readDataAllStops()
 objetoGraficar.createHistogram(dataAllStops)
 
-objetoGraficar= PulsesHistogram(5,500000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data5Stop.png")
+objetoGraficar= PulsesHistogram(5,2000000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data5Stop.png", False, "Stop 5 20000 pulses 24Mhz")
 dataOneStop = objetoGraficar.readDataOneStop()
 dataAllStops = objetoGraficar.readDataAllStops()
 objetoGraficar.createHistogram(dataOneStop)
 
-objetoGraficar= PulsesHistogram(4,500000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data4Stop.png")
+objetoGraficar= PulsesHistogram(4,2000000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data4Stop.png", False, "Stop 4 20000 pulses 24Mhz")
 dataOneStop = objetoGraficar.readDataOneStop()
 dataAllStops = objetoGraficar.readDataAllStops()
 objetoGraficar.createHistogram(dataOneStop)
 
-objetoGraficar= PulsesHistogram(3,500000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data3Stop.png")
+objetoGraficar= PulsesHistogram(3,2000000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data3Stop.png" ,False, "Stop 3 20000 pulses 24Mhz")
 dataOneStop = objetoGraficar.readDataOneStop()
 dataAllStops = objetoGraficar.readDataAllStops()
 objetoGraficar.createHistogram(dataOneStop)
 
-objetoGraficar= PulsesHistogram(2,500000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data2Stop.png")
+objetoGraficar= PulsesHistogram(2,2000000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data2Stop.png",False, "Stop 2 20000 pulses 24Mhz")
 dataOneStop = objetoGraficar.readDataOneStop()
 dataAllStops = objetoGraficar.readDataAllStops()
 objetoGraficar.createHistogram(dataOneStop)
 
-objetoGraficar= PulsesHistogram(1,500000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data1Stop.png")
-dataOneStop = objetoGraficar.readDataOneStop()
-dataAllStops = objetoGraficar.readDataAllStops()
-objetoGraficar.createHistogram(dataOneStop)
-
-
-objetoGraficar= PulsesHistogram(5,500000,200000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data5StopZoom.png")
-dataOneStop = objetoGraficar.readDataOneStop()
-dataAllStops = objetoGraficar.readDataAllStops()
-objetoGraficar.createHistogram(dataOneStop)
-
-objetoGraficar= PulsesHistogram(4,500000,200000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data4StopZoom.png")
-dataOneStop = objetoGraficar.readDataOneStop()
-dataAllStops = objetoGraficar.readDataAllStops()
-objetoGraficar.createHistogram(dataOneStop)
-
-objetoGraficar= PulsesHistogram(3,500000,200000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data3StopZoom.png")
-dataOneStop = objetoGraficar.readDataOneStop()
-dataAllStops = objetoGraficar.readDataAllStops()
-objetoGraficar.createHistogram(dataOneStop)
-
-objetoGraficar= PulsesHistogram(2,500000,200000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data2StopZoom.png")
-dataOneStop = objetoGraficar.readDataOneStop()
-dataAllStops = objetoGraficar.readDataAllStops()
-objetoGraficar.createHistogram(dataOneStop)
-
-objetoGraficar= PulsesHistogram(1,500000,200000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data1StopZoom.png")
+objetoGraficar= PulsesHistogram(1,2000000,4000000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data1Stop.png",False, "Stop 1 20000 pulses 24Mhz")
 dataOneStop = objetoGraficar.readDataOneStop()
 dataAllStops = objetoGraficar.readDataAllStops()
 objetoGraficar.createHistogram(dataOneStop)
 
 
-objetoGraficar= PulsesHistogram(5,300000,140000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/DataConfident0.3us.png")
+objetoGraficar= PulsesHistogram(5,200000,200000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data5StopZoom.png",False, "Stop 5 Zoom 20000 pulses 24Mhz")
+dataOneStop = objetoGraficar.readDataOneStop()
+dataAllStops = objetoGraficar.readDataAllStops()
+objetoGraficar.createHistogram(dataOneStop)
+
+objetoGraficar= PulsesHistogram(4,200000,200000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data4StopZoom.png",False, "Stop 4 Zoom 20000 pulses 24Mhz")
+dataOneStop = objetoGraficar.readDataOneStop()
+dataAllStops = objetoGraficar.readDataAllStops()
+objetoGraficar.createHistogram(dataOneStop)
+
+objetoGraficar= PulsesHistogram(3,200000,200000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data3StopZoom.png",False, "Stop 3 Zoom 20000 pulses 24Mhz")
+dataOneStop = objetoGraficar.readDataOneStop()
+dataAllStops = objetoGraficar.readDataAllStops()
+objetoGraficar.createHistogram(dataOneStop)
+
+objetoGraficar= PulsesHistogram(2,200000,200000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data2StopZoom.png",False, "Stop 2 Zoom 20000 pulses 24Mhz")
+dataOneStop = objetoGraficar.readDataOneStop()
+dataAllStops = objetoGraficar.readDataAllStops()
+objetoGraficar.createHistogram(dataOneStop)
+
+objetoGraficar= PulsesHistogram(1,200000,200000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/Data1StopZoom.png",False, "Stop 1 Zoom 20000 pulses 24Mhz")
+dataOneStop = objetoGraficar.readDataOneStop()
+dataAllStops = objetoGraficar.readDataAllStops()
+objetoGraficar.createHistogram(dataOneStop)
+
+
+objetoGraficar= PulsesHistogram(5,300000,144500000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/DataConfident0.3us.png",True, "g2 0.3 us bin size")
 dataOneStop = objetoGraficar.readDataOneStop()
 dataAllStops = objetoGraficar.readDataAllStops()
 objetoGraficar.createHistogram(dataAllStops)
 
-objetoGraficar= PulsesHistogram(5,200000,140000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/DataConfident0.2us.png")
+objetoGraficar= PulsesHistogram(5,200000,144500000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/DataConfident0.2us.png",True, "g2 0.2 us bin size")
 dataOneStop = objetoGraficar.readDataOneStop()
 dataAllStops = objetoGraficar.readDataAllStops()
 objetoGraficar.createHistogram(dataAllStops)
 
-objetoGraficar= PulsesHistogram(5,100000,140000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/DataConfident0.1us.png")
+objetoGraficar= PulsesHistogram(5,100000,144500000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/DataConfident0.1us.png",True, "g2 0.1 us bin size")
 dataOneStop = objetoGraficar.readDataOneStop()
 dataAllStops = objetoGraficar.readDataAllStops()
 objetoGraficar.createHistogram(dataAllStops)
 
-objetoGraficar= PulsesHistogram(5,50000,140000000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/DataConfident0.05us.png")
+objetoGraficar= PulsesHistogram(5,50000,144500000,'40000Pulsos24MHZ/Histograma4000024Mhz.txt',"40000Pulsos24MHZ/DataConfident0.05us.png",True, "g2 0.05 us bin size")
 dataOneStop = objetoGraficar.readDataOneStop()
 dataAllStops = objetoGraficar.readDataAllStops()
 objetoGraficar.createHistogram(dataAllStops)
@@ -174,5 +189,3 @@ objetoGraficar.createHistogram(dataAllStops)
 
 
 
-
-    
